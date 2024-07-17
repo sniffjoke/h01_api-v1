ae1f59b787fb6d4d8ec28313c506158e00d07600
sniffjoke <sniffjoke@gmail.com>
2024-07-17 22:20:27 +0200

    add app index and settings files, add first endpoint

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+
+export const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.status(200).json({version: '1.0'})
+})
---
+++ src/settings.ts
@@ -0,0 +1,10 @@
+import {config} from "dotenv";
+
+config()
+
+export const SETTINGS = {
+    PORT: process.env.PORT || 3003,
+    PATH: {
+        VIDEOS: '/videos'
+    }
+}
